b04efce78b92898d7482eacdcf87301238165d77
Melissa Tiangtae <melissa_tiangate@cmu.ac.th>
2024-12-06 15:15:00 +0700

    lab8_2_completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,6 +4,22 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double x){
+    
+    char result;
+    
+    if(x>90)
+        result = 'A';
+    else if(x>75)
+        result = 'B';
+    else if(x>60) 
+        result = 'C';
+    else if(x>45)
+        result = 'D';
+    else result = 'F';
+    
+    return result;
+}
 
 int main(){
 	//Input the number of students
@@ -18,8 +34,10 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin, name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
